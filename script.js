e71918ffd1362109115d4450089c4c9290909b93
jaklan-star <ok.jakelan@gmail.com>
2026-03-30 02:06:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 let finalURL="";
 
+document.getElementById("input").addEventListener("input", resolve);
+
 async function resolve(){
+
 let url=document.getElementById("input").value;
+
+if(!url) return;
+
 document.getElementById("status").innerText="Resolving...";
+
 let res=await fetch("/resolve?url="+encodeURIComponent(url));
+
 let data=await res.json();
+
 finalURL=data.url.replace(/flipkart.com/gi,"shopsy.in");
+
 document.getElementById("output").value=finalURL;
+
 document.getElementById("status").innerText="Done";
-}
 
-function copy(){
-let text=document.getElementById("output");
-text.select();
-document.execCommand("copy");
-}
-
-function openLink(){
-window.open(finalURL,"_blank");
-}
-
-function clearAll(){
-document.getElementById("input").value="";
-document.getElementById("output").value="";
-document.getElementById("status").innerText="";
 }
